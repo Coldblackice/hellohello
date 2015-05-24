cb93ec6daa7527036a31f43630d8cf18200674c1
coldblackice <coldblackice@gmail.com>
2015-05-24 06:27:13 -0700

    Update hellohello.py

--- hellohello.py
+++ hellohello.py
@@ -1,1 +1,26 @@
+def hellohello():
+    """
+    simple as sin
+    """
 
+    #name deetz
+    first = 'stewz'
+    last = 'kaleedz'
+    
+    first = input("First?:")
+    last = input("Last?:")
+
+    
+# let's print
+
+print "hi"
+"my"
+"name"
+"is"
+"Stewz"
+"god"
+"of"
+"jupiter"
+
+if(2 == 1 + 1):
+    hellohello()
